017c44ed337e8bdad60b56e6b29878fd7a210712
Sajib <sajib@appscode.com>
2017-02-01 10:04:17 +0600

    Solved: Fetch and Show Code School Badges Using Ajax

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,35 @@
 $(function() {
-
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/tigerworks.json',
+    dataType: 'jsonp',
+    success: function (resp) {
+      console.log('>>>',resp.courses.completed);
+      addCourses(resp.courses.completed);
+      }
+    });
 
+  function addCourses(courses) {
+    var $badges = $('#badges');
+    courses.forEach(function(course) {
+      var $course = $('<div />', {
+        'class': 'course'
+    }).appendTo($badges);
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+
+    $('<a />', {
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See Course'
+    }).appendTo($course);
+  })
+  }
 });
